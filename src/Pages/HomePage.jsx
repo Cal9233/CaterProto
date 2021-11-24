3aeef1513bb65a947f09e9c33c5d0e92b76ac86c
Cal <calvin.m71@hotmail.com>
2021-11-24 15:17:04 -0800

    header

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+        <h1>
+          Hello World
+        </h1>
+    </div>
+  )
+}
+
+export default HomePage
